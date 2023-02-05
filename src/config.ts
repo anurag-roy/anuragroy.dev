9cc5c2517b4a73ad32b5af37b311daa2e1a7c2a4
Anurag Roy <anuragroy@duck.com>
2023-02-05 16:40:34 +0530

    Add minimal content

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Anurag Roy - Full-stack TypeScript Developer';
+export const SITE_DESCRIPTION =
+  "Hey, I am Anurag! I'm a full stack engineer, specializing in TypeScript and Angular.";
