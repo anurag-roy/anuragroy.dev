3e108c502e7573f9bf213f9ef567b9fe6c220da7
Anurag Roy <anuragroy@duck.com>
2023-02-06 10:54:47 +0530

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anuragroy.dev/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
 });
